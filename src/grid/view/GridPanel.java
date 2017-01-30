397527c0da0461d9d0ad87de01eec3b44f69d9bd
thenaz <ammarznazir@gmail.com>
2017-01-30 09:14:56 -0700

    Created MyTwoDProject

---
+++ src/grid/view/GridPanel.java
@@ -0,0 +1,32 @@
+package grid.view;
+
+import javax.swing.JPanel;
+import grid.controller.GridController;
+
+public class GridPanel extends JPanel
+{
+	private JTextField rowField;
+	private JTextField columnField;
+	private JTextField inputField;
+	private JLabel rowLabel;
+	private JLabel columnLabel;
+	private JLabel submitButton;
+	private SpringLayout baseLayout;
+	private GridController baseController;
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+
+}
